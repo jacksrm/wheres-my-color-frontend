e83caf5bad4d3e5e947428c544d3b6eb4730eb7b
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-11-20 23:04:42 -0300

    Implementa Contexto de usuário

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,17 @@
-export interface ILoginContext {
-  userId: string;
-  email: string;
-  token: string;
-  username: string;
-  createdAt: string;
-  logIn: (data: ILoginData) => Promise<void>;
-}
-
 export interface ILoginData {
   email: string;
   password: string;
+}
+
+export interface ILoginContext {
+  token: string;
+  logIn: (data: ILoginData) => Promise<void>;
+}
+
+export interface IUserContext {
+  profilePicture: string;
+  userId: string;
+  email: string;
+  username: string;
+  createdAt: string;
 }
