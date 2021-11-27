b9f29d682b9cfd63eccbdd0a12b1fea9f6e1bf16
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-11-26 22:59:08 -0300

    fix estilo componente Initial
    Signed-off-by: Jacson Rodrigues <jacson.s.r1@gmail.com>

--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -2,39 +2,34 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { ButtonSimple, ButtonCustom } from '../../components/Buttons';
 import { FooterPage } from '../../components/FooterPage/index';
-import illustration from '../../images/Group 2042.svg';
+import illustration from '../../images/Group2042.svg';
+import logo from '../../images/logo.png';
 
 import './index.css';
 
 export const Initial: FC = () => (
   <main className="Initial">
-    <header className="navbar">
-      <h2 className="Title">Where’s My Collor?</h2>
-    </header>
-
-    <div className="image-container">
-      <img className="image" src={illustration} alt="illustration" />
-    </div>
-
-    <p className="Apresentation">
-      A melhor maneira de guardar as suas paletas de cores de forma rápida e
-      prática.
-    </p>
-
-    <Link to="/login">
-      <ButtonCustom>LOGIN</ButtonCustom>
-    </Link>
-    <Link to="/create">
-      <div className="create">
-        <ButtonSimple className="create">
-          Não tem conta?
-          {' '}
-          <strong>Cadastre-se</strong>
-        </ButtonSimple>
+    <section className="presentation">
+      <div className="content">
+        <img className="logo" src={logo} alt="logo" />
+        <h1>Where’s My Collor?</h1>
+        <p>
+          A melhor maneira de guardar as suas paletas de cores de forma rápida e
+          prática.
+        </p>
+        <Link to="/login">
+          <ButtonCustom>LOGIN</ButtonCustom>
+        </Link>
+        <Link to="/create">
+          <ButtonSimple className="create">
+            Não tem conta?
+            {' '}
+            <strong>Cadastre-se</strong>
+          </ButtonSimple>
+        </Link>
       </div>
-    </Link>
-
-    <p className="border-div" />
+      <img className="image" src={illustration} alt="illustration" />
+    </section>
 
     {/* criar componente container com slide sobre o projeto */}
 
