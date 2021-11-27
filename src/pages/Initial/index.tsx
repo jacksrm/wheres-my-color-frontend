a5f5fa9880924dd8ea0a66967cc4496081568d5b
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-11-26 23:25:03 -0300

    Fix do footer e alguns fix em Initial
    Signed-off-by: Jacson Rodrigues <jacson.s.r1@gmail.com>

--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -8,31 +8,31 @@ import logo from '../../images/logo.png';
 import './index.css';
 
 export const Initial: FC = () => (
-  <main className="Initial">
-    <section className="presentation">
-      <div className="content">
-        <img className="logo" src={logo} alt="logo" />
-        <h1>Where’s My Collor?</h1>
-        <p>
-          A melhor maneira de guardar as suas paletas de cores de forma rápida e
-          prática.
-        </p>
-        <Link to="/login">
-          <ButtonCustom>LOGIN</ButtonCustom>
-        </Link>
-        <Link to="/create">
-          <ButtonSimple className="create">
-            Não tem conta?
-            {' '}
-            <strong>Cadastre-se</strong>
-          </ButtonSimple>
-        </Link>
-      </div>
-      <img className="image" src={illustration} alt="illustration" />
-    </section>
-
-    {/* criar componente container com slide sobre o projeto */}
-
+  <>
+    <main className="Initial">
+      <section className="presentation">
+        <div className="content">
+          <img className="logo" src={logo} alt="logo" />
+          <h1>Where’s My Collor?</h1>
+          <p>
+            A melhor maneira de guardar as suas paletas de cores de forma rápida e
+            prática.
+          </p>
+          <Link to="/login">
+            <ButtonCustom>LOGIN</ButtonCustom>
+          </Link>
+          <Link to="/create">
+            <ButtonSimple className="create">
+              Não tem conta?
+              {' '}
+              <strong>Cadastre-se</strong>
+            </ButtonSimple>
+          </Link>
+        </div>
+        <img className="image" src={illustration} alt="illustration" />
+      </section>
+      {/* criar componente container com slide sobre o projeto */}
+    </main>
     <FooterPage />
-  </main>
+  </>
 );
